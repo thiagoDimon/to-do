3e6f1e4606cfaac0151718598b64ae29d06e8983
thiagoDimon <othiago.miranda@outlook.com>
2024-01-11 21:37:45 -0300

    Adiciona adição e edição de tarefas para o working e done

--- to-do-list/src/components/ToDoList.vue
+++ to-do-list/src/components/ToDoList.vue
@@ -18,7 +18,7 @@
           <v-btn
             color="primary"
             style="background-color: rgba(44, 111, 186, 0.25)"
-            @click="adicionarTarefa"
+            @click="adicionarTarefa(colunaTarefa)"
             >Adicionar</v-btn
           >
           <v-btn
@@ -49,7 +49,7 @@
           <v-btn
             color="primary"
             style="background-color: rgba(44, 111, 186, 0.25)"
-            @click="salvarEdicaoTarefa"
+            @click="salvarEdicaoTarefa(colunaTarefa)"
             >Salvar</v-btn
           >
           <v-btn
@@ -75,7 +75,7 @@
           <v-btn
             color="primary"
             style="background-color: rgba(44, 111, 186, 0.25)"
-            @click="deletarTarefa"
+            @click="deletarTarefa(colunaTarefa)"
             >Confirmar</v-btn
           >
           <v-btn
@@ -102,16 +102,27 @@
               density="comfortable"
               class="float-right"
               style="background: rgba(0, 0, 0, 0.1)"
-              @click="ref((dialogAdicionarTarefa = true))"
+              @click="
+                ref((dialogAdicionarTarefa = true)),
+                  ref((colunaTarefa = 'toDo'))
+              "
             >
             </v-btn>
           </v-card-title>
           <v-card-text>
-            <v-expansion-panels class="ga-2" style="border-radius: none">
+            <v-expansion-panels
+              class="ga-2"
+              style="border-radius: none"
+              @drop="onDrop($event, 'toDo')"
+              @dragenter.prevent
+              @dragover.prevent
+            >
               <v-expansion-panel
                 v-for="(tarefas, index) in toDos"
                 :key="tarefas"
                 style="background: rgba(0, 0, 0, 0.1)"
+                draggable="true"
+                @dragstart="startDrag($event, tarefas, index)"
               >
                 <div
                   class="posicao-botao-editar"
@@ -130,7 +141,8 @@
                         ref(
                           (descricaoTarefaAtribuida = tarefas.tarefa.descricao)
                         ),
-                        ref((indexTarefa = index))
+                        ref((indexTarefa = index)),
+                        ref((colunaTarefa = 'toDo'))
                     "
                   ></v-btn>
                   <v-btn
@@ -154,26 +166,144 @@
           </v-card-text>
         </v-card>
         <v-card class="tamanho-card" style="background: rgba(0, 0, 0, 0.1)">
-          <v-card-title> Working </v-card-title>
+          <v-card-title>
+            Working
+            <v-btn
+              icon="mdi-plus"
+              density="comfortable"
+              class="float-right"
+              style="background: rgba(0, 0, 0, 0.1)"
+              @click="
+                ref((dialogAdicionarTarefa = true)),
+                  ref((colunaTarefa = 'working'))
+              "
+            >
+            </v-btn>
+          </v-card-title>
           <v-card-text>
-            <v-expansion-panels class="ga-2">
-              <v-expansion-panel v-for="i in 3" :key="i">
-                <v-expansion-panel-title> Teste </v-expansion-panel-title>
-                <v-expansion-panel-text>
-                  Meu texto vai aqui
+            <v-expansion-panels
+              class="ga-2"
+              style="border-radius: none"
+              @drop="onDrop($event, 'working')"
+              @dragenter.prevent
+              @dragover.prevent
+            >
+              <v-expansion-panel
+                v-for="(tarefas, index) in working"
+                :key="tarefas"
+                style="background: rgba(0, 0, 0, 0.1)"
+                draggable="true"
+                @dragstart="startDrag($event, tarefas, index)"
+              >
+                <div
+                  class="posicao-botao-editar"
+                  style="
+                    background-color: rgba(0, 0, 0, 0.1);
+                    border-radius: 0px 5px 0px 0px;
+                  "
+                >
+                  <v-btn
+                    icon="mdi-pencil"
+                    density="comfortable"
+                    style="margin-right: 15px"
+                    @click="
+                      ref((dialogEditarTarefa = true)),
+                        ref((tituloTarefaAtribuida = tarefas.tarefa.titulo)),
+                        ref(
+                          (descricaoTarefaAtribuida = tarefas.tarefa.descricao)
+                        ),
+                        ref((indexTarefa = index)),
+                        ref((colunaTarefa = 'working'))
+                    "
+                  ></v-btn>
+                  <v-btn
+                    icon="mdi-delete"
+                    density="comfortable"
+                    style="margin-right: 15px"
+                    @click="
+                      ref((dialogDeletarTarefa = true)),
+                        ref((indexTarefa = index)),
+                        ref((colunaTarefa = 'working'))
+                    "
+                  ></v-btn>
+                </div>
+                <v-expansion-panel-title style="background-color: white">
+                  {{ tarefas.tarefa.titulo }}
+                </v-expansion-panel-title>
+                <v-expansion-panel-text style="background-color: white">
+                  {{ tarefas.tarefa.descricao }}
                 </v-expansion-panel-text>
               </v-expansion-panel>
             </v-expansion-panels>
           </v-card-text>
         </v-card>
         <v-card class="tamanho-card" style="background: rgba(0, 0, 0, 0.1)">
-          <v-card-title> Done </v-card-title>
+          <v-card-title>
+            Done
+            <v-btn
+              icon="mdi-plus"
+              density="comfortable"
+              class="float-right"
+              style="background: rgba(0, 0, 0, 0.1)"
+              @click="
+                ref((dialogAdicionarTarefa = true)),
+                  ref((colunaTarefa = 'done'))
+              "
+            >
+            </v-btn>
+          </v-card-title>
           <v-card-text>
-            <v-expansion-panels class="ga-2">
-              <v-expansion-panel v-for="i in 3" :key="i">
-                <v-expansion-panel-title> Teste </v-expansion-panel-title>
-                <v-expansion-panel-text>
-                  Meu texto vai aqui, mesmo
+            <v-expansion-panels
+              class="ga-2"
+              style="border-radius: none"
+              @drop="onDrop($event, 'done')"
+              @dragenter.prevent
+              @dragover.prevent
+            >
+              <v-expansion-panel
+                v-for="(tarefas, index) in done"
+                :key="tarefas"
+                style="background: rgba(0, 0, 0, 0.1)"
+                draggable="true"
+                @dragstart="startDrag($event, tarefas, index)"
+              >
+                <div
+                  class="posicao-botao-editar"
+                  style="
+                    background-color: rgba(0, 0, 0, 0.1);
+                    border-radius: 0px 5px 0px 0px;
+                  "
+                >
+                  <v-btn
+                    icon="mdi-pencil"
+                    density="comfortable"
+                    style="margin-right: 15px"
+                    @click="
+                      ref((dialogEditarTarefa = true)),
+                        ref((tituloTarefaAtribuida = tarefas.tarefa.titulo)),
+                        ref(
+                          (descricaoTarefaAtribuida = tarefas.tarefa.descricao)
+                        ),
+                        ref((indexTarefa = index)),
+                        ref((colunaTarefa = 'done'))
+                    "
+                  ></v-btn>
+                  <v-btn
+                    icon="mdi-delete"
+                    density="comfortable"
+                    style="margin-right: 15px"
+                    @click="
+                      ref((dialogDeletarTarefa = true)),
+                        ref((indexTarefa = index)),
+                        ref((colunaTarefa = 'done'))
+                    "
+                  ></v-btn>
+                </div>
+                <v-expansion-panel-title style="background-color: white">
+                  {{ tarefas.tarefa.titulo }}
+                </v-expansion-panel-title>
+                <v-expansion-panel-text style="background-color: white">
+                  {{ tarefas.tarefa.descricao }}
                 </v-expansion-panel-text>
               </v-expansion-panel>
             </v-expansion-panels>
@@ -190,16 +320,19 @@ import { ref } from "vue";
 interface Tarefa {
   titulo: string;
   descricao: string;
+  coluna: string;
 }
-
 interface ToDo {
   tarefa: Tarefa;
 }
 
 const toDos: any = ref([]);
+const working: any = ref([]);
+const done: any = ref([]);
 
 const tituloTarefa = ref("");
 const descricaoTarefa = ref("");
+const colunaTarefa = ref("");
 const tituloTarefaAtribuida = ref("");
 const descricaoTarefaAtribuida = ref("");
 const indexTarefa = ref(0);
@@ -208,43 +341,86 @@ const dialogAdicionarTarefa = ref(false);
 const dialogEditarTarefa = ref(false);
 const dialogDeletarTarefa = ref(false);
 
-function adicionarTarefa() {
+function adicionarTarefa(coluna: string) {
   const novaTarefa: ToDo = {
     tarefa: {
       titulo: tituloTarefa.value,
       descricao: descricaoTarefa.value,
+      coluna: colunaTarefa.value,
     },
   };
 
+  if (coluna === "toDo") {
+    ref(toDos.value.push(novaTarefa));
+  } else if (coluna === "working") {
+    ref(working.value.push(novaTarefa));
+  } else {
+    ref(done.value.push(novaTarefa));
+  }
+
   ref((tituloTarefa.value = ""));
   ref((descricaoTarefa.value = ""));
-  ref(toDos.value.push(novaTarefa));
 
   return ref((dialogAdicionarTarefa.value = false));
 }
 
-function salvarEdicaoTarefa() {
+function salvarEdicaoTarefa(coluna: string) {
   const tarefaEditada: ToDo = {
     tarefa: {
       titulo: tituloTarefaAtribuida.value,
       descricao: descricaoTarefaAtribuida.value,
+      coluna: colunaTarefa.value,
     },
   };
 
-  ref((toDos.value[indexTarefa.value] = tarefaEditada));
+  if (coluna === "toDo") {
+    ref((toDos.value[indexTarefa.value] = tarefaEditada));
+  } else if (coluna === "working") {
+    ref((working.value[indexTarefa.value] = tarefaEditada));
+  } else {
+    ref((done.value[indexTarefa.value] = tarefaEditada));
+  }
 
   return ref((dialogEditarTarefa.value = false));
 }
 
-function deletarTarefa() {
-  if (toDos.value.length > 1) {
-    ref(toDos.value.splice(indexTarefa.value, 1));
+function deletarTarefa(coluna: string) {
+  if (coluna === "toDo") {
+    if (toDos.value.length > 1) {
+      ref(toDos.value.splice(indexTarefa.value, 1));
+    } else {
+      ref((toDos.value = []));
+    }
+    return ref((dialogDeletarTarefa.value = false));
+  } else if (coluna === "working") {
+    if (working.value.length > 1) {
+      ref(working.value.splice(indexTarefa.value, 1));
+    } else {
+      ref((working.value = []));
+    }
     return ref((dialogDeletarTarefa.value = false));
   } else {
-    ref((toDos.value = []));
+    if (done.value.length > 1) {
+      ref(done.value.splice(indexTarefa.value, 1));
+    } else {
+      ref((done.value = []));
+    }
     return ref((dialogDeletarTarefa.value = false));
   }
 }
+
+const startDrag = (event: any, tarefa: any, index: any) => {
+  console.log("tarefa", tarefa);
+  event.dataTransfer.dropEffect = "move";
+  event.dataTransfer.effectAllowed = "move";
+  event.dataTransfer.setData("indexTarefa", index);
+};
+
+const onDrop = (event: any, colunaTarefa: string) => {
+  const indexTarefa = event.dataTransfer.getData("indexTarefa");
+  const tarefa = toDos.value.find((tarefa: any) => tarefa.index == indexTarefa);
+  tarefa.colunaTarefa = colunaTarefa;
+};
 </script>
 
 <style>
