00921ae9cd7d1f766b030c397b5f2aa887c0b6fe
thiagoDimon <othiago.miranda@outlook.com>
2024-01-29 19:07:05 -0300

    Resolve reload página

--- to-do-list/src/App.vue
+++ to-do-list/src/App.vue
@@ -1,7 +1,12 @@
 <template>
-  <router-view/>
+  <router-view />
 </template>
 
 <script lang="ts" setup>
-  //
+if (!sessionStorage.getItem("reloaded")) {
+  sessionStorage.setItem("reloaded", "true");
+  window.location.href = "/";
+} else {
+  sessionStorage.removeItem("reloaded");
+}
 </script>
